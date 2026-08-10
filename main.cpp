4bc01a42e6e262d360eb410612014dd05250e8a2
sarthakmhase2007-ctrl <sarthak.mhase2007@gmail.com>
2026-08-10 21:27:22 +0530

    Edited by Sarthak

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//Created by Atharva
 int addNumbers(int num1,int num2){
     return num1+num2;
 }
@@ -12,6 +12,22 @@ int subtractNumbers(int num1,int num2){
 int multiplyNumbers(int num1,int num2){
     return num1*num2;
 }
+//Edited by Sarthak
+float dividenumbers(int x,int y){
+    return x/y;
+}
+
+int gcd(int x, int y)
+{
+    while (y != 0)
+    {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+    return x;
+}
+
 
 int main(){
     int a=12;
@@ -19,6 +35,8 @@ int main(){
     cout<<"Sum: "<<addNumbers(a,b)<<endl;
     cout<<"Difference: "<<subtractNumbers(a,b)<<endl;
     cout<<"Product: "<<multiplyNumbers(a,b)<<endl;
-    
+    //Edited by Sarthak
+    cout<<"Division :"<<dividenumbers(a,b)<<endl;
+    cout<<"GCD :"<<gcd(a,b)<<endl;
     return 0;
 }
